bff16615495f60f01eb86a0c65e7111a946a1341
shaiz2021 <mshahzaibkk2003@gmail.com>
2024-08-20 16:09:46 +0500

    asavvvv

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-const header = document.querySelector("header");
-window.addEventListener("scroll", function () {
-    header.classList.toggle("sticky", window.scrollY > 0);
-});
-
 let menu = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
